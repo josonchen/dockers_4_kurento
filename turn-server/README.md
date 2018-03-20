c636d3174f6be709742293315c6a9abf876a9b54
josonchen <josonchen@sina.com>
2018-03-20 23:55:39 +0800

    update
    some small changes

--- turn-server/README.md
+++ turn-server/README.md
@@ -1,4 +1,5 @@
 # Docker image for TURN server
+
 A Docker container with the [Coturn TURN server](https://github.com/coturn/coturn)
 
-sudo docker run -d -p 3478:3478 -p 3478:3478/udp --restart=always zolochevska/turn-server username password realm
+sudo docker run -d -p 3478:3478 -p 3478:3478/udp --restart=always joson/turn-server theusername thepassword therealm
--- turn-server/deploy-turnserver.sh
+++ turn-server/deploy-turnserver.sh
@@ -2,11 +2,13 @@ echo $1
 echo $2
 echo $3
 
+
 internalIp="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 echo "listening-port=3478
 tls-listening-port=5349
+
 listening-ip="$internalIp"
 relay-ip="$internalIp"
 external-ip="$externalIp"
