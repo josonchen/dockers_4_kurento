67183ded131e9138f4df22d4bd34cd8920487df5
josonchen <josonchen@sina.com>
2018-03-21 14:00:55 +0800

    bugs
    [KurentoClient]  Exception connecting to WebSocket server ws://host/kurento: connection timed out:

--- kurento-media-server/deploy-kms.sh
+++ kurento-media-server/deploy-kms.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 set -e
 
-echo $1
-echo $2
-echo $3
+#echo $1
+#echo $2
+#echo $3
 
 KMS_STUN_IP="$1"
 KMS_STUN_PORT="$2"
@@ -23,7 +23,7 @@ if [ -n "$KMS_STUN_IP" -a -n "$KMS_STUN_PORT" ]; then
 fi
 
 # Remove ipv6 local loop until ipv6 is supported
-cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
+#cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
 
 
 cat  /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
@@ -34,17 +34,17 @@ export GST_DEBUG='3,Kurento*:4,kms*:4,rtpendpoint:4,webrtcendpoint:4'
 cd /app/build-Debug
 
 
-echo "kurento-media-server server running. stunServerAddress= "$1"  stunServerPort= "$2" turnURL= "$3" "
+echo "kurento-media-server server  will run. stunServerAddress= "$1"  stunServerPort= "$2" turnURL= "$3" "
 
-echo "kurento-media-server server running. stunServerAddress= "$KMS_STUN_IP"  stunServerPort= "$KMS_STUN_PORT" turnURL= "$KMS_TURN_URL" "
+#echo "kurento-media-server server running. stunServerAddress= "$KMS_STUN_IP"  stunServerPort= "$KMS_STUN_PORT" turnURL= "$KMS_TURN_URL" "
 
 
-exec kurento-media-server/server/kurento-media-server \
+kurento-media-server/server/kurento-media-server \
   --modules-path=. \
   --modules-config-path=./config \
   --conf-file=./config/kurento.conf.json \
   --gst-plugin-path=.
 
-echo "kurento-media-server server running. stunServerAddress= "$1"  stunServerPort= "$2" turnURL= "$3" "
+# echo "kurento-media-server server running. stunServerAddress= "$1"  stunServerPort= "$2" turnURL= "$3" "
 
 echo "kurento-media-server server running. stunServerAddress= "$KMS_STUN_IP"  stunServerPort= "$KMS_STUN_PORT" turnURL= "$KMS_TURN_URL" "
