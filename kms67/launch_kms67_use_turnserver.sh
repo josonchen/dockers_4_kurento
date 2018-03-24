a58d9ace742ea12583377725af26ecf9ba738215
josonchen <josonchen@sina.com>
2018-03-24 23:23:41 +0800

    process with or without parameter
    #launch_kms67_use_turnserver.sh
    # can process with or without parameter

--- kms67/launch_kms67_use_turnserver.sh
+++ kms67/launch_kms67_use_turnserver.sh
@@ -1,25 +1,22 @@
-echo $1
-echo $2
-echo $3
-
-stunServerAddress=$1
-stunServerPort=$2
-turnURL=$3
-
-# backup the original ini file
-
-if [ -f /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini_original ]
-then
-    cp /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini_original
+if [ $# -ge 3 ] ; then 
+	echo $1
+	echo $2
+	echo $3
+	stunServerAddress=$1
+	stunServerPort=$2
+	turnURL=$3
+	# backup the original ini file
+	if [ -f /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini_original ]
+	then
+	    cp /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini_original
+	fi
+	echo \"
+		stunServerAddress=\"$stunServerAddress\"
+		stunServerPort=\"$stunServerPort\"
+		turnURL=\"$turnURL\"
+	\"| tee /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+exit 1; 
 fi
-
-echo \"
-stunServerAddress=\"$stunServerAddress\"
-stunServerPort=\"$stunServerPort\"
-turnURL=\"$turnURL\"
-
-\"| tee /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-
 
 export GST_DEBUG='3,Kurento*:4,kms*:4,rtpendpoint:4,webrtcendpoint:4'
 
