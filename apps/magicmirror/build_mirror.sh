e65c6240edd0529d7ddb4afba04454d5616a9efd
josonchen <josonchen@sina.com>
2018-03-25 00:48:17 +0800

    update
    modi

--- apps/magicmirror/build_mirror.sh
+++ apps/magicmirror/build_mirror.sh
@@ -1,1 +1,11 @@
 # build_mirror.sh
+
+cd /app/web_apps
+
+git clone https://github.com/Kurento/kurento-tutorial-java.git
+
+cd /app/web_apps/kurento-tutorial-java/kurento-magic-mirror
+
+git checkout 6.7.1
+
+# mvn compile exec:java
