87cefa831161d78fd356e9db69ba95023a6098f1
josonchen <josonchen@sina.com>
2018-03-24 20:56:33 +0800

    add build

---
+++ kms67/build_km67.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/Kurento/kms-omni-build.git \
+  && cd kms-omni-build \
+  && git submodule init \
+  && git submodule update --recursive --remote
+
+REF=master
+for d in $(find . -maxdepth 1 -mindepth 1 -type d)
+do pushd $d ; git checkout "$REF" ; popd ; done
+
+TYPE=Debug
+mkdir build-$TYPE \
+  && cd build-$TYPE \
+  && cmake -DCMAKE_BUILD_TYPE=$TYPE -DCMAKE_VERBOSE_MAKEFILE=ON .. \
+  && make
