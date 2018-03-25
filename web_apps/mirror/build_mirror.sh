9ba2272a338e0dafef3312f0b35f0d94ffe1834a
josonchen <josonchen@sina.com>
2018-03-25 11:25:43 +0800

    checkout the hiphop branch

--- web_apps/mirror/build_mirror.sh
+++ web_apps/mirror/build_mirror.sh
@@ -8,7 +8,9 @@ git clone -b  hiphop https://github.com/josonchen/kurento-tutorial-java.git
 
 cd /app/web_apps/kurento-tutorial-java/kurento-magic-mirror
 
-git checkout 6.7.1
+git checkout hiphop
+
+#git checkout 6.7.1
 
 # compiled ,after create the docker
 mvn compile 
